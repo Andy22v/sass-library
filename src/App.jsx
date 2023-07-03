0663f411b050f5aa88dfce44bddcf42d80d5c38a
Andy <andy@MBP-di-Andy.station>
2023-07-03 18:23:15 +0200

    chore:project-sass

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Service</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
