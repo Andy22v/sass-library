29f62981d426e9d9d1f41e7ec807d87f30661e8d
Andy <andy@MBP-di-Andy.station>
2023-07-04 18:44:49 +0200

    chore: column

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import "./App.scss";
 
 function App() {
@@ -11,6 +10,87 @@ function App() {
           <li>Service</li>
         </ul>
       </div>
+      <div className="col-container">
+        <div className="col-1">col-1</div>
+        <div className="col-2">col-1</div>
+        <div className="col-1">col-1</div>
+        <div className="col-1">col-1</div>
+        <div className="col-1">col-1</div>
+        <div className="col-1">col-1</div>
+        <div className="col-1">col-1</div>
+        <div className="col-1">col-1</div>
+        <div className="col-1">col-1</div>
+        <div className="col-1">col-1</div>
+        <div className="col-1">col-1</div>
+        <div className="col-1">col-1</div>
+      </div>
+
+      {/* <div className="col-container">
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+        <div className="col-xs-1">col-2</div>
+      </div>
+      <div className="col-container">
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+        <div className="col-sm-1">col-3</div>
+      </div>
+      <div className="col-container">
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+        <div className="col-md-1">col-4</div>
+      </div> */}
+      {/* <div className="col-container">
+        <div className="col-5"></div>
+      </div>
+      <div className="col-container">
+        <div className="col-6"></div>
+      </div>
+      <div className="col-container">
+        <div className="col-7"></div>
+      </div>
+      <div className="col-container">
+        <div className="col-8"></div>
+      </div>
+      <div className="col-container">
+        <div className="col-9"></div>
+      </div>
+      <div className="col-container">
+        <div className="col-10"></div>
+      </div>
+      <div className="col-container">
+        <div className="col-11"></div>
+      </div>
+      <div className="col-container">
+        <div className="col-12"></div>
+      </div> */}
     </div>
   );
 }
